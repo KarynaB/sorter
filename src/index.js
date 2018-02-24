e71d4fb2394313e5bbc4657717f5013a7013994c
Karyna <36641408+KarynaB@users.noreply.github.com>
2018-02-24 14:51:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
 class Sorter {
+  
   constructor() {
     // your implementation
+    this.name = 'Sorter';
   }
 
   add(element) {
     // your implementation
+    var add = [];
+    for ( var i = 0; i < element.length; i++ ) {
+      add.push( element[ i ]);
+    }
+    return add;
   }
 
   at(index) {
     // your implementation
+    for (var i = 0; i < this.add().length; ++i)
+        {
+          index = this.add()[i];
+              return index;
+       }
   }
 
   get length() {
     // your implementation
+    var a = this.add().length;
+    return a; 
   }
 
   toArray() {
     // your implementation
+   var b = [];
+   b = this.add();
+  return b;
   }
 
   sort(indices) {
     // your implementation
+  
   }
 
   setComparator(compareFunction) {
     // your implementation
+    
   }
 }
 
